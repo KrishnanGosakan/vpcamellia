4391220ed11a45265d741b7ad1fdd6d1ee1c48e5
krishnan <krishnan.gosakan@gmail.com>
2020-02-01 16:35:28 +0530

    replaced build.sh with makefile

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-rm -f encrypt.o
-rm -f modes.o
-rm -f vpCamellia.o
-rm -f camellia
-
-gcc -I../header ../src/encrypt.c -mavx2 -c
-gcc -I../header ../src/modes.c -mavx2 -c
-gcc -I../header ../src/vpCamellia.c -mavx2 -c
-gcc -o camellia encrypt.o modes.o vpCamellia.o
